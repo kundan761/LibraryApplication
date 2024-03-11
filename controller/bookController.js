ba424c827d2546c61a25cc645c2452dc28a03514
Kundan Kumar Suraj <kundankumarsuraj761@gmail.com>
2024-03-12 01:37:17 +0530

    Update bookController.js

--- controller/bookController.js
+++ controller/bookController.js
@@ -1,3 +1,4 @@
+const auth = require("../middleware/auth");
 const Book = require("../model/Book");
 
 exports.getAllBooks = async (req, res) => {
@@ -28,8 +29,7 @@ exports.getAllBooks = async (req, res) => {
   }
 };
 
-
-exports.createBook = async (req, res) => {
+exports.createBook = auth, async (req, res) => {
   try {
     const newBook = await Book.create(req.body);
     res.status(201).json(newBook);
@@ -38,23 +38,23 @@ exports.createBook = async (req, res) => {
   }
 };
 
-exports.updateBook = async (req, res) =>{
-    const {bookId} = req.body;
-    const payload = req.body;
-    try {
-        const updatedBook = await Book.findByIdAndUpdate({_id:bookId}, payload)
-        res.status(200).json(updatedBook);
-    } catch (error) {
-        res.status(500).json({ msg: "Something went wrong" });
-    }
-}
+exports.updateBook = auth, async (req, res) => {
+  const { bookId } = req.body;
+  const payload = req.body;
+  try {
+    const updatedBook = await Book.findByIdAndUpdate({ _id: bookId }, payload);
+    res.status(200).json(updatedBook);
+  } catch (error) {
+    res.status(500).json({ msg: "Something went wrong" });
+  }
+};
 
-exports.deleteBook = async (req, res) =>{
-    const {bookId} = req.body;
-    try {
-        const deletedBook = await Book.findByIdAndDelete({_id:bookId})
-        res.status(200).json({msg:'Deleted Successfully'});
-    } catch (error) {
-        res.status(500).json({ msg: "Something went wrong" });
-    }
-}
+exports.deleteBook = auth, async (req, res) => {
+  const { bookId } = req.body;
+  try {
+    const deletedBook = await Book.findByIdAndDelete({ _id: bookId });
+    res.status(200).json({ msg: "Deleted Successfully", deletedBook });
+  } catch (error) {
+    res.status(500).json({ msg: "Something went wrong" });
+  }
+};
